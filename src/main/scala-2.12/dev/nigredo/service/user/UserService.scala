7ec31ce6366c7944768c2cba19c51f98a4df586a
Andrii Ivanov <a.ivanov@levi9.com>
2017-02-07 16:06:37 +0200

    Fix types

--- src/main/scala-2.12/dev/nigredo/service/user/UserService.scala
+++ src/main/scala-2.12/dev/nigredo/service/user/UserService.scala
@@ -1,6 +1,5 @@
 package dev.nigredo.service.user
 
-import dev.nigredo.dao.Dao.Mongo.{createUser, findUserById, isEmailExists, updateUser}
 import dev.nigredo.domain.models
 import dev.nigredo.domain.models.User.{ExistingUser, NewUser, UpdatedUser, UserId}
 import dev.nigredo.domain.models._
@@ -12,9 +11,10 @@ private[service] object UserService {
 
   import dev.nigredo.service.Service._
   import com.github.t3hnar.bcrypt._
+  import dev.nigredo.dao.Dao.Mongo.{createUser, findUserById, isEmailExists, updateUser}
 
   private[this] val onCreate =
-    create[CreateUserDto, NewUser](dto => User(Name(dto.name), Email(dto.email), models.Password(dto.password.bcrypt)))(UserValidator(isEmailExists))(createUser)
+    create[CreateUserDto, NewUser](dto => User(Name(dto.name), Email(dto.email), models.Password(dto.password.bcrypt)))(UserValidator(isEmailExists))
   private[this] val onUpdate =
     update[UserId, UpdateUserDto, ExistingUser, UpdatedUser](findUserById)((x: ExistingUser) => (dto: UpdateUserDto) =>
       x.update((dto.name.map(Name), dto.email.map(Email), dto.password.map(x => models.Password(x.bcrypt)), dto.active.map(x => Activation(x)))))(UserValidator(isEmailExists))(updateUser)
